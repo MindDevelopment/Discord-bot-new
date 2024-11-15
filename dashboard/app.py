c24b4e68590228c4a6f2cf425cf25331d9ace505
MindDevelopment <mindofdevelopment@gmail.com>
2024-11-15 03:34:05 +0100

    1

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, session, render_template, request
 import json
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Zorg ervoor dat je een geheime sleutel hebt voor sessies
@@ -17,6 +18,28 @@ CLIENT_ID = config['client_id']
 CLIENT_SECRET = config['client_secret']
 REDIRECT_URI = config['redirect_uri']
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+# PM2 commando's
+def pm2_start():
+    return subprocess.Popen(['pm2', 'start', 'bot.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def pm2_stop():
+    subprocess.run(['pm2', 'stop', 'bot.py'], check=True)
+
+def pm2_restart():
+    subprocess.run(['pm2', 'restart', 'bot.py'], check=True)
+
+def get_console_output():
+    """ Haal de laatste uitvoer op van de bot via PM2 logs """
+    process = subprocess.Popen(['pm2', 'logs', 'discord-bot'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output, errors = process.communicate()
+    return output
+
+=======
+>>>>>>> 7ae39fdac9dde89f18ca24226200b68f9ef4c7f3
+>>>>>>> Stashed changes
 # Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,7 +63,12 @@ def index():
     if 'user' not in session:
         return redirect(url_for('login'))
     user = session['user']
+<<<<<<< Updated upstream
     return render_template('index.html', user=user)
+=======
+    console_output = get_console_output()  # Haal de console-output op
+    return render_template('index.html', user=user, console_output=console_output)
+>>>>>>> Stashed changes
 
 # Logout route
 @app.route('/logout')
